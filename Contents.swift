b28d998cbb083e3084ef194bbb66f87bc48cec38
Joel <jpeddhi@contractor.lb.com>
2023-02-20 15:35:06 +0530

    Dictionaries Added

--- Contents.swift
+++ Contents.swift
@@ -163,3 +163,21 @@ var songs2 = ["Today was a Fairytale", "Welcome to New York", "Fifteen"]
 var bothSongs = songs1 + songs2
 /// You can also use += to add and assign, like this:
 bothSongs += ["Everything has Changed"]
+
+// MARK:- DICTIONARIES
+/// As you've seen, Swift arrays are a collection where you access each item using a numerical index, such as songs[0]. Dictionaries are another common type of collection, but they differ from arrays because they let you access values based on a key you specify.
+
+/// To give you an example, let's imagine how we might store data about a person in an array:
+var person = ["Taylor", "Alison", "Swift", "December", "taylorswift.com"]
+/// To read out that person's middle name, we'd use person[1], and to read out the month they were born we'd use person[3]. This has a few problems, not least that it's difficult to remember what index number is assigned to each value in the array! And what happens if the person has no middle name? Chances are all the other values would move down one place, causing chaos in your code.
+
+/// With dictionaries we can re-write this to be far more sensible, because rather than arbitrary numbers you get to read and write values using a key you specify. For example:
+var persons = [
+                "first": "Taylor",
+                "middle": "Alison",
+                "last": "Swift",
+                "month": "December",
+                "website": "taylorswift.com"
+            ]
+persons["middle"]
+persons["month"]
