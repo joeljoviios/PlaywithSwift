9d5653f370100987903d2897cbf1b7a6ba9e8078
Joel <jpeddhi@contractor.lb.com>
2023-02-20 14:46:02 +0530

    variables, simple data types, and string interpolation

---
+++ Contents.swift
@@ -0,0 +1,108 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// MARK:- VARIABLES AND CONSTANTS
+
+var name = "Jona"
+name = "JONA"
+
+let constantName = "JONA"
+// constantName = "jona" // xcode complains: Cannot assign to value: 'constantName' is a 'let' constant
+
+// MARK:- TYPES OF DATA
+
+// String
+// Int
+
+// TYPE SAFETY:-
+
+//name = 25 // xcode complains: Cannot assign value of type 'Int' to type 'String'
+var age: Int
+// age = "1234" // xcode complains: Cannot assign value of type 'String' to type 'Int'
+
+// Float
+// Double
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+
+// Boolean
+/// Swift has a built-in data type that can store whether a value is true or false, called a Bool, short for Boolean. Bools don't have space for "maybe" or "perhaps", only absolutes: true or false.
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var missABeat: Bool
+missABeat = false
+
+// TYPE INFERENCE:-
+/// Using type annotations wisely
+/// There are two ways to tell Swift what type of data a variable holds: assign a value when you create the variable, or use a type annotation.
+var string = "String"
+var integer = 25
+var double = -86.783333
+var boolean = true
+/// This technique is called type inference, because Swift can infer what data type should be used for a variable by looking at the type of data you want to put in there.
+
+// MARK:- OPERATORS
+/// + to add, - to subtract, * to multiply, / to divide, = to assign value, and so on.
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+/// += is an operator that means "add then assign to." In our case it means "take the current value of b, add 10 to it, then put the result back into b." As you might imagine, -= does the same but subtracts rather than adds. So, that code will show 10, 20, 10 in the results pane.
+
+/// Some of these operators apply to other data types. As you might imagine, you can add two doubles together like this:
+var x = 1.1
+var y = 2.2
+var c = x + y
+
+/// When it comes to strings, + will join them together. For example:
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+
+/// One more common operator you’ll see is called modulus, and is written using a percent symbol: %. It means “divide the left hand number evenly by the right, and return the remainder.” So, 9 % 3 returns 0 because 3 divides evenly into 9, whereas 10 % 3 returns 1, because 10 divides by 3 three times, with remainder 1.
+
+9 % 3
+10 % 3
+
+// COMPARISION OPERATORS:-
+
+c > 3
+c >= 3
+c > 4
+c < 4
+
+///  ==, meaning "is equal to." For example:
+
+var equals = "Tim McGraw"
+equals == "Tim McGraw"
+
+/// There's one more operator I want to introduce you to, and it's called the "not" operator: !. Yes, it's just an exclamation mark. This makes your statement mean the opposite of ==. For example:
+var stayLate = true
+stayLate
+!stayLate
+
+/// You can also use ! with = to make != or "not equal". For example:
+var notEquals = "Tim McGraw"
+notEquals == "Tim McGraw"
+notEquals != "Tim McGraw"
+
+// STRING INTERPOLATION:-
+/// This is a fancy name for what is actually a very simple thing: combining variables and constants inside a string.
+
+var interPolation = "Tim McGraw"
+"Your name is \(interPolation)"
+var stringAge = 25
+var stringLatitude = 36.166667
+
+"Your name is \(interPolation), your age is \(stringAge), and your latitude is \(stringLatitude)"
