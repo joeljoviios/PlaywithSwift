4667d61b07f44a11ca76e874a5783b505fa95729
Joel <jpeddhi@contractor.lb.com>
2023-02-22 12:28:50 +0530

    Access Control Added

--- Contents.swift
+++ Contents.swift
@@ -1141,3 +1141,20 @@ print(TaylorFan.favoriteSong)
 /// So, a Taylor Swift fan has a name and age that belongs to them, but they all have the same favorite song.
 
 /// Because static methods belong to the struct itself rather than to instances of that struct, you can't use it to access any non-static properties from the struct.
+
+// MARK:- ACCESS CONTROL
+/// Access control lets you specify what data inside structs and classes should be exposed to the outside world, and you get to choose four modifiers:
+
+/// Public: this means everyone can read and write the property.
+/// Internal: this means only your Swift code can read and write the property. If you ship your code as a framework for others to use, they won’t be able to read the property.
+/// File Private: this means that only Swift code in the same file as the type can read and write the property.
+/// Private: this is the most restrictive option, and means the property is available only inside methods that belong to the type, or its extensions.
+/// Most of the time you don't need to specify access control, but sometimes you'll want to explicitly set a property to be private because it stops others from accessing it directly. This is useful because your own methods can work with that property, but others can't, thus forcing them to go through your code to perform certain actions.
+
+/// To declare a property private, just do this:
+class TaylorFann {
+    fileprivate var name: String?
+}
+let taylorFan = TaylorFann()
+taylorFan.name = "fenny" // xcode complains: 'name' is inaccessible due to 'private' protection level
+/// If you want to use “file private” access control, just write it as one word like so: fileprivate.
